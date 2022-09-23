128a7add548d9b5a0e720ccd866c2a114d362a95
dplochcoder <daniel.ploch.coder@gmail.com>
2022-09-23 00:06:16 -0400

    Fixup non-vanilla placements

--- MoreDoors/Rando/Vanilla.cs
+++ MoreDoors/Rando/Vanilla.cs
@@ -17,8 +17,12 @@ namespace MoreDoors.Rando
 
         private static void PlaceVanillaItems(On.UIManager.orig_StartNewGame orig, UIManager self, bool permaDeath, bool bossRush)
         {
-            if (RandomizerMod.RandomizerMod.RS?.GenerationSettings == null) return;
-            if (!RandoInterop.IsEnabled || RandomizerMod.RandomizerMod.RS.GenerationSettings.PoolSettings.Keys) return;
+            if (RandomizerMod.RandomizerMod.RS?.GenerationSettings == null || !RandoInterop.IsEnabled
+                || RandomizerMod.RandomizerMod.RS.GenerationSettings.PoolSettings.Keys)
+            {
+                orig(self, permaDeath, bossRush);
+                return;
+            }
 
             List<AbstractPlacement> placements = new();
             foreach (var door in DoorData.DoorNames)
