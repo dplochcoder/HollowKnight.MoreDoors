42163bcfef3ee0509a985a4cc60e5e41b56457fa
dplochcoder <daniel.ploch.coder@gmail.com>
2024-02-04 15:57:27 -0500

    Implement sprite dimmer for door lighting

---
+++ MoreDoors/IC/SpriteDimmer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MoreDoors.IC;
+
+internal record SpriteDimmer : ItemChanger.Deployer
+{
+    public string TargetGameObject;
+    public float AlphaMultiplier;
+
+    public override GameObject Instantiate() => throw new System.NotImplementedException();
+
+    public override GameObject Deploy()
+    {
+        var target = GameObject.Find(TargetGameObject);
+        if (target == null) return null;
+
+        var spriteRenderer = target.GetComponent<SpriteRenderer>();
+        if (spriteRenderer == null) return null;
+
+        var color = spriteRenderer.color;
+        spriteRenderer.color = new(color.r, color.g, color.b, color.a * AlphaMultiplier);
+        return null;
+    }
+}
